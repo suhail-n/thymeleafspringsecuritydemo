c4f042b4c487abfad72fd474a4dd7af77db964bd
suhail-n <suhail.nooristani@gmail.com>
2023-12-26 19:51:31 -0500

    reformatted file

--- src/main/java/com/example/thymeleafsecuritydemo/events/listners/InitialDataLoader.java
+++ src/main/java/com/example/thymeleafsecuritydemo/events/listners/InitialDataLoader.java
@@ -21,75 +21,75 @@ import com.example.thymeleafsecuritydemo.repository.UserRepository;
 @Component
 public class InitialDataLoader {
 
-        boolean alreadySetup = false;
+    boolean alreadySetup = false;
 
-        private final UserRepository userRepository;
-        private final RoleRepository roleRepository;
-        private final PrivilegeRepository privilegeRepository;
-        private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PrivilegeRepository privilegeRepository;
+    private final PasswordEncoder passwordEncoder;
 
-        public InitialDataLoader(UserRepository userRepository, RoleRepository roleRepository,
-                        PrivilegeRepository privilegeRepository, PasswordEncoder passwordEncoder) {
-                this.userRepository = userRepository;
-                this.roleRepository = roleRepository;
-                this.privilegeRepository = privilegeRepository;
-                this.passwordEncoder = passwordEncoder;
-        }
+    public InitialDataLoader(UserRepository userRepository, RoleRepository roleRepository,
+                             PrivilegeRepository privilegeRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.privilegeRepository = privilegeRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-        @EventListener
-        @Transactional
-        public void onApplicationEvent(ContextRefreshedEvent event) {
-                // The ContextRefreshedEvent may be fired multiple times depending on how many
-                // contexts we have configured
-                // in our application. And we only want to run the setup once.
-                if (alreadySetup)
-                        return;
-                Privilege readPrivilege = createPrivilegeIfNotExist("READ_PRIVILEGE");
-                Privilege writePrivilege = createPrivilegeIfNotExist("WRITE_PRIVILEGE");
-                // ROLE_ prefix will be added by spring
-                createRoleIfNotExist("ADMIN", Arrays.asList(readPrivilege, writePrivilege));
-                createRoleIfNotExist("USER", List.of(readPrivilege));
+    @EventListener
+    @Transactional
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        // The ContextRefreshedEvent may be fired multiple times depending on how many
+        // contexts we have configured
+        // in our application. And we only want to run the setup once.
+        if (alreadySetup)
+            return;
+        Privilege readPrivilege = createPrivilegeIfNotExist("READ_PRIVILEGE");
+        Privilege writePrivilege = createPrivilegeIfNotExist("WRITE_PRIVILEGE");
+        // ROLE_ prefix will be added by spring
+        createRoleIfNotExist("ADMIN", Arrays.asList(readPrivilege, writePrivilege));
+        createRoleIfNotExist("USER", List.of(readPrivilege));
 
-                // ROLE_ prefix will be added by spring
-                createUserIfNotExist("user", "user@email.com", "password", "USER");
-                createUserIfNotExist("admin", "admin@email.com", "password", "ADMIN");
-        }
+        // ROLE_ prefix will be added by spring
+        createUserIfNotExist("user", "user@email.com", "password", "USER");
+        createUserIfNotExist("admin", "admin@email.com", "password", "ADMIN");
+    }
 
-        private void createUserIfNotExist(String username, String email, String password, String roleName) {
-                this.userRepository.findByUsername(username)
-                                .or(
-                                                () -> this.roleRepository
-                                                                .findByName(roleName)
-                                                                .map(
-                                                                                role -> UserEntity
-                                                                                                .builder()
-                                                                                                .username(username)
-                                                                                                .email(email)
-                                                                                                .password(this.passwordEncoder
-                                                                                                                .encode(password))
-                                                                                                .roles(List.of(role))
-                                                                                                .build())
-                                                                .map(this.userRepository::save));
-        }
-
-        private void createRoleIfNotExist(String roleName, Collection<Privilege> privileges) {
-                this.roleRepository.findByName(roleName).or(
-                                () -> Optional.of(Role
+    private void createUserIfNotExist(String username, String email, String password, String roleName) {
+        this.userRepository.findByUsername(username)
+                .or(
+                        () -> this.roleRepository
+                                .findByName(roleName)
+                                .map(
+                                        role -> UserEntity
                                                 .builder()
-                                                .name(roleName)
-                                                .privileges(privileges)
-                                                .build()))
-                                .map(this.roleRepository::save);
-        }
+                                                .username(username)
+                                                .email(email)
+                                                .password(this.passwordEncoder
+                                                        .encode(password))
+                                                .roles(List.of(role))
+                                                .build())
+                                .map(this.userRepository::save));
+    }
 
-        private Privilege createPrivilegeIfNotExist(String privilegeName) {
-                return this.privilegeRepository.findByName(privilegeName).or(
-                                () -> Optional.of(Privilege
-                                                .builder()
-                                                .name(privilegeName)
-                                                .build()))
-                                .map(this.privilegeRepository::save)
-                                .get();
-        }
+    private void createRoleIfNotExist(String roleName, Collection<Privilege> privileges) {
+        this.roleRepository.findByName(roleName).or(
+                        () -> Optional.of(Role
+                                .builder()
+                                .name(roleName)
+                                .privileges(privileges)
+                                .build()))
+                .map(this.roleRepository::save);
+    }
+
+    private Privilege createPrivilegeIfNotExist(String privilegeName) {
+        return this.privilegeRepository.findByName(privilegeName).or(
+                        () -> Optional.of(Privilege
+                                .builder()
+                                .name(privilegeName)
+                                .build()))
+                .map(this.privilegeRepository::save)
+                .get();
+    }
 
 }
